161823798496100fecccc061b8f17ed2a66b49ce
huverac <huver.acevedo.sierra@gmail.com>
2019-09-07 12:41:45 -0500

    Ejercicio Uno, para la longitud de dos números

---
+++ ejercicio/sentenciaIf.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace sentenciaIf
+{
+    class Program{
+        static void Main(string[] args){
+            int numero1;
+
+            Console.WriteLine("Digite el número");
+            numero1 = int.Parse(Console.ReadLine());
+
+        }
+    }
+}s
---
+++ ejercicioUno/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ejercicioUno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            Console.WriteLine(" ------------------------------------------\n| PROGRAMA PARA LA LONGITUD DE DOS DIGITOS |\n ------------------------------------------");
+            Console.WriteLine("Digite un número");
+            numero = int.Parse(Console.ReadLine());
+            if(numero > 9){
+                Console.WriteLine("El número tiene dos dígitos");
+            }else{
+                Console.WriteLine("El número no tiene dos digitos");
+            }
+            Console.ReadKey();
+        }
+    }
+}
