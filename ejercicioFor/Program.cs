6e58389ce79e275a3f2e1305d2e66a7cb0d66af0
huverac <huver.acevedo.sierra@gmail.com>
2019-09-09 11:50:59 -0500

    Ejercicio

---
+++ ejercicioFor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ejercicioFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
